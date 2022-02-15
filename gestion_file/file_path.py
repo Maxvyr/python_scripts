6081576567bfad0d07fc11912030dee722eef8ed
Maxvyr <maxvid30@gmail.com>
2022-02-15 18:26:13 +0100

    :sparkles: gestion file path

---
+++ gestion_file/file_path.py
@@ -0,0 +1,22 @@
+import os.path
+
+filename = os.path.join("rep","sample.txt")
+
+if os.path.exists(filename):
+    print(f"file exist path : {filename}")
+    f = open(filename, "r")
+    txt = f.read()
+    print(txt)
+    f.close()
+else:
+    print("File doesn't exist")
+
+
+#create  folder
+# os.mkdir("rep")
+
+#remove folder
+# os.rmdir("rep")
+
+#remove fil
+# os.remove(filename)
