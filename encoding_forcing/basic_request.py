3dce035f02913bd0d6353114e9def0b6d0ebec0a
Maxvyr <maxvid30@gmail.com>
2022-02-15 19:40:24 +0100

    :sparkles: simple function with requests

---
+++ encoding_forcing/basic_request.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+res = requests.get("https://codeavecjonathan.com/res/programmation.txt")
+if(res.status_code == 200):
+    res.encoding = "utf-8"
+    print(res.text)
+else:
+    print(f"ERROR {res.status_code}")
+
+res = requests.get("https://google.com/no_file.html")
+if(res.status_code == 200):
+    res.encoding = "utf-8"
+    print(res.text)
+else:
+    print(f"ERROR {res.status_code}")
+
+res = requests.get("https://jsonplaceholder.typicode.com/photos")
+if(res.status_code == 200):
+    res.encoding = "utf-8"
+    photos = json.loads(res.text)
+    print(f"Total = {len(photos)} Photos")
+else:
+    print(f"ERROR {res.status_code}")
---
+++ encoding_forcing/dlwd_file_requests.py
@@ -0,0 +1,12 @@
+import requests
+
+res = requests.get("https://konachan.com/sample/679d359386dc110303c371ca82a8bec5/Konachan.com/sample.jpg")
+
+if res.status_code == 200:
+    print("Start Write file")
+    f = open("waifu.jpg", "wb")
+    f.write(res.content)
+    f.close
+    print("Finish write file")
+else:
+    print(f"ERROR {res.status_code}")
