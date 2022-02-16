0230be6e66cc7a0825a01796051ef743056b891a
Maxvyr <maxvid30@gmail.com>
2022-02-16 21:28:29 +0100

    :sparkles: playwright + vs4 for scrap rasp price

--- scraping/price_rasp.py
+++ scraping/price_rasp.py
@@ -1,27 +1,31 @@
-
-
-
-import asyncio
-from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 
 PRICE_GOAL = 44
 URL_RASP = "https://www.kubii.fr/cartes-raspberry-pi/2771-nouveau-raspberry-pi-4-modele-b-2gb-0765756931175.html?search_query=Pi4&results=111"
+message_email = f"""
+<h1>Hello</h1>
 
-async def main():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto(url=URL_RASP)
-        print(await page.title())
-        price_recover = await page.evaluate_handle("document.querySelector('span[itemprop=price]').innerHTML")
-        print(f"Rasp price is => " + price_recover);
-        price_pi_4 = int(price_recover[0:2])
+    <p>You can buy a raspeberry now</p>
+    <a href={URL_RASP}>LINK</a>
 
-        if(price_pi_4 < PRICE_GOAL):  
-            print(f"You can buy")
-        else:
-            print(f"Nope to high")
-            
-        await browser.close()
+<h2>Great, See ya</h2>
+"""
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(url=URL_RASP)
+    print(page.title())
+    body = page.inner_html(".product")
+    soup = BeautifulSoup(body, "html.parser")
+    price_recover = soup.find("span",{'id': 'our_price_display'}).text
+    print(f"Rasp price is => {price_recover}");
+    price_pi_4 = int(price_recover[0:2])
+    if(price_pi_4 < PRICE_GOAL):  
+        print(f"You can buy")
+        #TODO send email
+    else:
+        print(f"Nope to high")
         
-asyncio.run(main())
