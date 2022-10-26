2e6f2e75412ced83bc333d4ce481292d6898af63
maxvyr <maxvid30@gmail.com>
2022-10-26 14:24:39 +0200

    ♻️ reformat code

--- ffmpeg_script/mp4_to_mp3.py
+++ ffmpeg_script/mp4_to_mp3.py
@@ -3,5 +3,5 @@ import os
 base_dir = "/Users/mvidalinc/Downloads/"
 mp4_file = base_dir + "video.mp4"
 mp3_file = base_dir + "audio.mp3"
-cmd = "ffmpeg -i {} -q:a 0 -map a  {}".format(mp4_file, mp3_file)
+cmd = "ffmpeg -i {} -q:a 0 -map a {}".format(mp4_file, mp3_file)
 os.system(cmd)
