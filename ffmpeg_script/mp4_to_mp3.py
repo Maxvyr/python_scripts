41d183729fed942ddf6c92baf9ee4be823cc1af9
maxvyr <60290207+Maxvyr@users.noreply.github.com>
2023-03-10 18:02:53 +0100

    🙈 modif base dir

--- ffmpeg_script/mp4_to_mp3.py
+++ ffmpeg_script/mp4_to_mp3.py
@@ -1,6 +1,8 @@
 import os
+from pathlib import Path
 
-base_dir = "/Users/mvidalinc/Downloads/"
+base_dir = path_to_download_folder = str(
+    os.path.join(Path.home(), "Downloads/"))
 mp4_file = base_dir + "video.mp4"
 mp3_file = base_dir + "audio.mp3"
 cmd = "ffmpeg -i {} -q:a 0 -map a {}".format(mp4_file, mp3_file)
